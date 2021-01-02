ff213a5e946334f631b2def57546883e756891ab
Bryan <bradcede@espol.edu.ec>
2021-01-01 20:40:30 -0500

    Actualizado

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async(category)=>{
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=9Vvbt7fq8FPiMJCMtQRHz0L0QGiJd5Qs&q=${encodeURI(category)}&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=9Vvbt7fq8FPiMJCMtQRHz0L0QGiJd5Qs&q=${encodeURI(category)}&limit=10`;
     const resp = await fetch(url);
     const {data} = await resp.json();
     const gifs = data.map((img)=>{
